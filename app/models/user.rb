4b17b97df178491d3362306661dbbc62d3c0fe5b
nazrul <nazrulku07@gmail.com>
2015-08-07 00:13:17 +0600

    add user name to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,11 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   def forem_name
-    'Test User'
+    if name.present?
+      name
+    else
+      'Anonymous'
+    end
   end
 
   def forem_email
