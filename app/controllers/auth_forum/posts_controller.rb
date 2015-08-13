88fa173ad08de67747b3772d5133ed7cad550ba2
nazrul <nazrulku07@gmail.com>
2015-08-13 21:59:55 +0600

    fixed post show issue

--- app/controllers/auth_forum/posts_controller.rb
+++ app/controllers/auth_forum/posts_controller.rb
@@ -2,6 +2,7 @@ require_dependency "auth_forum/application_controller"
 
 module AuthForum
   class PostsController < ApplicationController
+    before_action :set_post, only: [:show, :edit, :update, :destroy]
 
     def index
       @posts = Post.all
@@ -40,6 +41,10 @@ module AuthForum
     end
 
     private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_post
+        @post = Post.find(params[:id])
+      end
 
       def post_params
         params.require(:post).permit(:title, :author, :description)
