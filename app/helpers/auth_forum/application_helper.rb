bfe10a4e13e1feddc4cecfafeb164a39c1152829
nazrul <nazrulku07@gmail.com>
2015-08-20 00:50:21 +0600

    design form validation message

--- app/helpers/auth_forum/application_helper.rb
+++ app/helpers/auth_forum/application_helper.rb
@@ -19,5 +19,19 @@ module AuthForum
       @devise_mapping ||= Devise.mappings[:user]
     end
 
+    def form_validation_message(obj)
+      if obj.errors.any?
+       html = "<div id='error_explanation'>"
+       html << "<h2> #{pluralize(@order.errors.count, 'error')} prohibited this post from being saved:</h2> <ul>"
+       obj.errors.full_messages.each do |message|
+         html << "<li> <i class='fa fa-times-circle'></i> #{message} </li>"
+       end
+        html << '</ul></div>'
+      else
+        html = ''
+      end
+      raw(html)
+    end
+
   end
 end
