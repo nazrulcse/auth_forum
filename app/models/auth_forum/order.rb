8672cc3ddcc4963c69340626643487f9dea3f1dd
nazrul <nazrulku07@gmail.com>
2015-08-19 23:02:30 +0600

    update order information

--- app/models/auth_forum/order.rb
+++ app/models/auth_forum/order.rb
@@ -9,6 +9,7 @@ module AuthForum
     attr_accessor :exp_month, :start_year, :card_cvv, :card_num
     has_many :line_items
     belongs_to :user
+    after_create :update_billing_address
     validates :card_holder_name, :presence => true
     validates :card_num, :presence => true
     validates :exp_month, :presence => true
@@ -16,19 +17,33 @@ module AuthForum
     validates :card_cvv, :presence => true
     validates :first_name, :presence => true
     validates :last_name, :presence => true
-    validates :b_address, :presence => true
-    validates :b_country, :presence => true
-    validates :b_city, :presence => true
-    validates :b_zip, :presence => true
-    validates :s_address, :presence => true , :unless => :same_address?
-    validates :s_country, :presence => true , :unless => :same_address?
-    validates :s_city, :presence => true , :unless => :same_address?
-    validates :s_zip, :presence => true , :unless => :same_address?
+    validates :b_address, :presence => true, :if => :same_billing_address?
+    validates :b_country, :presence => true, :if => :same_billing_address?
+    validates :b_city, :presence => true, :if => :same_billing_address?
+    validates :b_zip, :presence => true, :if => :same_billing_address?
+    validates :s_address, :presence => true
+    validates :s_country, :presence => true
+    validates :s_city, :presence => true
+    validates :s_zip, :presence => true
 
     def add_to_line_item(current_cart)
       current_cart.line_items.each do |item|
         line_items << item
       end
+    end
+
+    def update_billing_address
+      if same_address
+         self.b_address = s_address
+         self.b_country = s_country
+         self.b_city = s_city
+         self.b_zip = s_zip
+        self.save
+      end
+    end
+
+    def same_billing_address?
+      !same_address
     end
 
     def total_item
